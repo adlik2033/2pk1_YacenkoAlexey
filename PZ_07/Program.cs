94e405b7eacd85ab10308e63b4c71e7daccf28a4
adlik2033 <adlik2033@yandex.ru>
2023-09-30 16:49:33 +0500

    добавлена седьмая практика

---
+++ PZ_07/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[,]
+            {
+
+            { 11, 41, 35 },
+            { 43, 44, 123 },
+            { 7, 555, 21 },
+            { 11, 41, 12 }
+
+            };
+
+
+
+
+            int minSumRowIndex = 0;
+            int minSum = GetRowSum(matrix, 0);
+
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                int sum = GetRowSum(matrix, i);
+                if (sum < minSum)
+                {
+                    minSum = sum;
+                    minSumRowIndex = i;
+                }
+            }
+
+            Console.WriteLine("Строка с минимальной суммой элементов: " + minSumRowIndex);
+        }
+
+        static int GetRowSum(int[,] matrix, int rowIndex)
+        {
+            int sum = 0;
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[rowIndex, j];
+            }
+
+            return sum;
+        }
+    }
+}
+
