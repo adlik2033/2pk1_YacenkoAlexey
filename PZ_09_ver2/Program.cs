687eab8a5e53646c8f8edc09d40b2d124cf29ea1
adlik2033 <adlik2033@yandex.ru>
2023-11-19 13:47:17 +0500

    добавлена 9 практика. В прошлый раз не добавилась.

--- PZ_09_ver2/Program.cs
+++ PZ_09_ver2/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace PZ_09_ver2
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите любой текст: ");
+            string inputString = Console.ReadLine();
+            string[] words = inputString.Split(' ');
+            // Разделяем строку на слова
+
+            Array.Sort(words, (x, y) => y.Length.CompareTo(x.Length));
+            // Сортируем слова по убыванию длины
+
+            string outputString = string.Join(" ", words.Select(word => new string(word.Reverse().ToArray())));
+            // Переворачиваем каждое слово и объединяем их обратно в строку
+
+            Console.WriteLine($"Итоговый текст: {outputString}");
         }
+
     }
 }
