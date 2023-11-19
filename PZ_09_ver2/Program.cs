2cec11cb61c2e5937cd95d4f508fbdd349309a28
adlik2033 <adlik2033@yandex.ru>
2023-11-19 13:42:36 +0500

    добавлена девятая практика. В прошлый раз не добавилась.

---
+++ PZ_09_ver2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PZ_09_ver2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
