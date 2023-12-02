3f298f1b0913e74a9e631067ab254d36819b4ced
adlik2033 <adlik2033@yandex.ru>
2023-12-02 13:43:30 +0500

    добавлены 11 и 12 практики

---
+++ PZ_11/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            double x = 0;
+            double y = 0;
+            Console.WriteLine("Решение уравнения ax+bx+c=0, но a != 0");
+            Console.WriteLine("Введите значение A:");
+            Console.WriteLine("A ---> ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение B:");
+            Console.WriteLine("B ---> ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение C:");
+            Console.WriteLine("C ---> ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            GetResult(a, b, c, out x, out y);
+            Console.WriteLine("Ответ: ");
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+
+        static void GetResult(double a, double b, double c, out double x, out double y)
+        {
+
+            double d = 0;
+            double z = 0;
+            d = ((b * b) - 4 * a * c);
+            z = Math.Sqrt(d);
+            x = ((-b) + z) / 2 * a;
+            y = ((-b) - z) / 2 * a;
+
+        }
+    }
+}
