910694bdcec22fbecb71aa2b07e57d8b34ec4184
adlik2033 <adlik2033@yandex.ru>
2023-12-07 20:31:21 +0500

    добавлена 14 практика.

---
+++ PZ_14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "file.txt";
+        // file.txt - тестовый текстовый фаил, который находится в директории проекта. Там есть и пустые строки, и строки с текстом,
+        // и строки чисто с вопросом, и пробел с вопросом... все случаи. Проверяем работоспособность программы.
+
+        // Открываем файл для чтения
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            // Создаем объект StreamReader для чтения текста из файла
+            using (StreamReader reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+
+                // Читаем содержимое файла построчно
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Проверяем, оканчивается ли строка вопросительным или восклицательным знаком
+                    if (line.TrimEnd().EndsWith("?") || line.TrimEnd().EndsWith("!"))
+                    {
+                        // Выводим строку на консоль
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
